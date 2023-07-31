5a3a00c3991a573a3214f67ade791ca41eb563d3
GeekROS <15300555770>
2023-08-01 01:35:02 +0800

    1.0.0

--- hardware/main/src/main.c
+++ hardware/main/src/main.c
@@ -93,11 +93,9 @@ void Task_Manage(void)
 #define SYSTEM_TASK_PRIO 2
 #define SYSTEM_TASK_SIZE 256
 static TaskHandle_t System_Task_Handler;
-
 #define SERIAL_TASK_PRIO 3
 #define SERIAL_TASK_SIZE 1024
 static TaskHandle_t Serial_Task_Handler;
-
 #define USER_TASK_PRIO 4
 #define USER_TASK_SIZE 1024
 static TaskHandle_t User_Task_Handler;
