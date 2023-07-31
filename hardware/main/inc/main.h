b956818979c1b6ea22cac2e6650777236ab258c1
GeekROS <15300555770>
2023-08-01 01:23:24 +0800

    1.0.0

--- hardware/main/inc/main.h
+++ hardware/main/inc/main.h
@@ -13,19 +13,19 @@
 #include "utils.h"
 
 #define MAIN_TASK_PRIO 1
-#define MAIN_TASK_SIZE 512
+#define MAIN_TASK_SIZE 1024
 static TaskHandle_t Main_Task_Handler;
 
 #define SYSTEM_TASK_PRIO 2
-#define SYSTEM_TASK_SIZE 512
+#define SYSTEM_TASK_SIZE 256
 static TaskHandle_t System_Task_Handler;
 
 #define SERIAL_TASK_PRIO 3
-#define SERIAL_TASK_SIZE 512
+#define SERIAL_TASK_SIZE 1024
 static TaskHandle_t Serial_Task_Handler;
 
 #define USER_TASK_PRIO 4
-#define USER_TASK_SIZE 512
+#define USER_TASK_SIZE 1024
 static TaskHandle_t User_Task_Handler;
 
 uint32_t Serial_Task_Buffer_Len = 512;
